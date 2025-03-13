e97220fa9371e32f965791c0628eb6c619beffb8
Vinoda123 <14vinodakn@gmail.com>
2025-03-13 11:37:57 +0530

    payment class

---
+++ src/test/java/project/PaymentPage.java
@@ -0,0 +1,11 @@
+package project;
+
+public class PaymentPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("new class created");
+	}
+
+}
